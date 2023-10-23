7dcccfb3abe8810e1a255a0fdbea79c55049ceb6
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-10-23 23:50:45 +0200

    feat: define SQLModel models
    - Define Subject, Center, MRIExam, and MRIVolume models
    - Establish relationships between models (one-to-one, one-to-many)
    - Implement unit tests (creation of models)

--- oxytcmri/tests.py
+++ oxytcmri/tests.py
@@ -1,10 +1,104 @@
 # tests.py
 
 from oxytcmri import settings
+from oxytcmri.models import Subject, Center, MRIExam, MRIVolume
+import pytest
+from sqlmodel import Session, SQLModel, create_engine, select
+
+
+class TestModels:
+    """
+        A class containing unit tests for the Oxy-TC MRI models.
+
+        Attributes:
+        - database_session: A fixture providing a database session for testing.
+
+        Methods:
+        - test_create_subject: Test the creation of a Subject model.
+        - test_create_center: Test the creation of a Center model.
+        - test_create_mri_exam: Test the creation of an MRIExam model.
+        - test_create_mri_volume: Test the creation of an MRIVolume model.
+
+        Note: Ensure to use the provided database session for testing (i.e. the fixture).
+        """
+    @pytest.fixture
+    def database_session(self):
+        """
+            Fixture providing a database session for testing.
+
+            Returns:
+            - session: A SQLAlchemy session for database interactions.
+        """
+        engine = create_engine("sqlite://", connect_args={"check_same_thread": False})
+        SQLModel.metadata.create_all(engine)
+        with Session(engine) as session:
+            yield session
+            SQLModel.metadata.drop_all(engine)
+
+    def test_create_subject(self, database_session):
+        # Test creating a Subject
+        grenoble = Center(id=1, name="Grenoble")
+        subject1 = Subject(id="subject_id_1", subject_type="healthy_volunteer", center=grenoble)
+        database_session.add(subject1)
+        database_session.commit()
+
+        db_subject = database_session.execute(select(Subject)).scalar_one()
+        assert db_subject is not None
+        assert db_subject.id == "subject_id_1"
+        assert db_subject.subject_type == "healthy_volunteer"
+        assert db_subject.center.id == 1
+
+    def test_create_center(self, database_session):
+        # Test creating a Center
+        center = Center(id=1, name="Grenoble")
+        database_session.add(center)
+        database_session.commit()
+
+        db_center = database_session.execute(select(Center)).scalar_one()
+        assert db_center is not None
+        assert db_center.id == 1
+        assert db_center.name == "Grenoble"
+
+    def test_create_mri_exam(self, database_session):
+        # Test creating an MRIExam
+        subject = Subject(id="subject_id_2", subject_type="patient", center_id=2)
+        database_session.add(subject)
+
+        mri_exam = MRIExam(subject=subject)
+        database_session.add(mri_exam)
+        database_session.commit()
+
+        db_mri_exam = database_session.execute(select(MRIExam)).scalar_one()
+        assert db_mri_exam is not None
+        assert db_mri_exam.id == 1
+        assert db_mri_exam.subject.id == "subject_id_2"
+
+    def test_create_mri_volume(self, database_session):
+        # Test creating an MRIVolume
+        paris = Center(id=3, name="Paris")
+        subject3 = Subject(id="subject_id_3", subject_type="test_patient", center=paris)
+        database_session.add(subject3)
+
+        mri_exam = MRIExam(subject=subject3)
+        database_session.add(mri_exam)
+
+        mri_volume = MRIVolume(name="T1", filepath="/path/to/t1.nii.gz", exam=mri_exam)
+        database_session.add(mri_volume)
+
+        database_session.commit()
+
+        db_mri_volume = database_session.execute(select(MRIVolume)).scalar_one()
+        db_mri_exam = database_session.execute(select(MRIExam)).scalar_one()
+        assert db_mri_volume is not None
+        assert db_mri_volume.name == "T1"
+        assert db_mri_volume.filepath == "/path/to/t1.nii.gz"
+        assert db_mri_volume.exam.subject.center == paris
+        assert db_mri_exam.volumes == [db_mri_volume]
 
 
 class TestSettings:
     """Test suite for verifying the behavior of the settings module"""
+
     def test_read_test_variable(self):
         """Verify that the "test_variable" in the [test] section
         of the  settings file "settings.toml" is read correctly"""
