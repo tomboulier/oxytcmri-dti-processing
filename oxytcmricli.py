df093d84517eed9fc2ceafb130c8179e0820a37b
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-12-22 18:43:51 +0100

    feat: add structural MRI (T1/T2/FLAIR) to database

--- oxytcmricli.py
+++ oxytcmricli.py
@@ -14,7 +14,8 @@ def import_data(
         subjects_list_csv_filepath: str = typer.Option(None, "--subjects-list", "-sl", help="Path to the CSV file "
                                                                                             "containing the subjects "
                                                                                             "list"),
-        mri_data_path: str = typer.Option(None, "--mri-data-path", "-m", help="Path to the MRI data folder"),
+        dti_data_path: str = typer.Option(None, "--dti-data-path", "-dti", help="Path to the DTI (Diffusion Tensor Imaging) data folder"),
+        structural_mri_data_path: str = typer.Option(None, "--structural-mri-data-path", "-mri", help="Path to the structural MRI (T1/T2/FLAIR) data folder"),
         database_url: str = typer.Option(None, "--database-url", "-d", help="URL of the database"),
 ):
     """
@@ -30,9 +31,13 @@ def import_data(
 
     # Import data from the CSV files
     subjects_list_csv_filepath = settings.paths.SubjectsList if subjects_list_csv_filepath is None else subjects_list_csv_filepath
-    clinical_data_path = settings.paths.ClinicalData if mri_data_path is None else mri_data_path
-    mri_data_path = settings.paths.MRIData if mri_data_path is None else mri_data_path
-    database_controller.import_data(subjects_list_csv_filepath, clinical_data_path, mri_data_path)
+    clinical_data_path = settings.paths.ClinicalData if dti_data_path is None else dti_data_path
+    dti_data_path = settings.paths.DTIDataPath if dti_data_path is None else dti_data_path
+    structural_mri_data_path = settings.paths.StructuralDataPath if structural_mri_data_path is None else structural_mri_data_path
+    database_controller.import_data(subjects_list_csv_filepath,
+                                    clinical_data_path, 
+                                    dti_data_path,
+                                    structural_mri_data_path)
 
     typer.echo("Data imported successfully.")
 
