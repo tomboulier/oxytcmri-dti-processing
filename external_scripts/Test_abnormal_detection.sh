d4131b1c7784a28621936610fbde8621253db94f
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-02-14 11:11:28 +0100

    refactor: improve shell script readability

--- external_scripts/Test_abnormal_detection.sh
+++ external_scripts/Test_abnormal_detection.sh
@@ -1,115 +1,71 @@
+#!/bin/bash
 
+# Check if the required number of arguments is provided
+if [ "$#" -lt 7 ]; then
+    echo "Usage: $0 <folder> <threshold_mode> <devcyto> <devvaso> <volcyto> <volvaso>"
+    exit 1
+fi
 
+# Input arguments
 f=$1
+THRESHOLD_MODE=$2
+DEVCYTO=$3
+DEVVASO=$4
+VOLCYTO=$5
+VOLVASO=$6
 
+# Constants
 LFD_DISTANCE_THRESHOLD=2
 PCSF_THRESHOLD=0.05
-
 CODE=Test
-THRESHOLD_MODE=$3
-DEVCYTO=$4
-DEVVASO=$5
-VOLCYTO=$6
-VOLVASO=$7
 
+# File paths
 PCSF=${f}/tmp/S002_PCSF_TX_MD.nii.gz
 LFD=${f}/tmp/S002_LFD_TX_MD.nii.gz
 T1_SEG=${f}/tmp/S002_T1SEG_TX_MD.nii.gz
 SUPRATENTORIAL_MASK=${f}/tmp/S002_SupratentorialMask.nii.gz
-#=====
-# new:
-# ====
-#T1_PARENCHYMA_MASK=${f}/${CODE}_T1ParenchymaMask.nii.gz
-#c3d ${T1_SEG} -threshold 3 4 1 0 -o ${T1_PARENCHYMA_MASK} # this is conservative; add class 5 for full parenchyma
-
-#PCSF_MASK=${f}/${CODE}_PCSFMask.nii.gz
-#c3d ${PCSF} -threshold ${PCSF_THRESHOLD} inf 1 0 -o ${PCSF_MASK}
-
-#LFD_MASK=${f}/${CODE}_LFDMask.nii.gz
-#c3d ${LFD} -threshold ${LFD_DISTANCE_THRESHOLD} inf 1 0 -o ${LFD_MASK}
-
-#TEST_MASK=${f}/${CODE}_Mask.nii.gz
-#c3d ${SUPRATENTORIAL_MASK} ${T1_PARENCHYMA_MASK} -multiply -o ${TEST_MASK}
-#c3d ${LFD_MASK} ${TEST_MASK} -multiply -o ${TEST_MASK}
-#c3d ${TEST_MASK} ${PCSF_MASK} -threshold 0 0 1 0 -multiply -o ${TEST_MASK}
-
-#ATLAS_2=${f}/tmp/S002_Atlas2.nii.gz
-#ATLAS_3=${f}/tmp/S002_Atlas3.nii.gz
-#ATLAS_4=${f}/tmp/S002_Atlas4.nii.gz
-#ATLAS_5=${f}/tmp/S002_Atlas5.nii.gz
-#ATLAS_6=${f}/tmp/S002_Atlas6.nii.gz
-#ATLAS_7=${f}/tmp/S002_Atlas7.nii.gz
-
-#TEST_ATLAS_2=${f}/${CODE}_Atlas2.nii.gz
-#TEST_ATLAS_3=${f}/${CODE}_Atlas3.nii.gz
-#TEST_ATLAS_4=${f}/${CODE}_Atlas4.nii.gz
-#TEST_ATLAS_5=${f}/${CODE}_Atlas5.nii.gz
-#TEST_ATLAS_6=${f}/${CODE}_Atlas6.nii.gz
-#TEST_ATLAS_7=${f}/${CODE}_Atlas7.nii.gz
-
-#c3d ${ATLAS_2} ${TEST_MASK} -multiply -o ${TEST_ATLAS_2}
-#c3d ${ATLAS_3} ${TEST_MASK} -multiply -o ${TEST_ATLAS_3}
-#c3d ${ATLAS_4} ${TEST_MASK} -multiply -o ${TEST_ATLAS_4}
-#c3d ${ATLAS_5} ${TEST_MASK} -multiply -o ${TEST_ATLAS_5}
-#c3d ${ATLAS_6} ${TEST_MASK} -multiply -o ${TEST_ATLAS_6}
-#c3d ${ATLAS_7} ${TEST_MASK} -multiply -o ${TEST_ATLAS_7}
-
-TEST_ATLAS_2=${f}/tmp/S002_Atlas2.nii.gz
-TEST_ATLAS_3=${f}/tmp/S002_Atlas3.nii.gz
-TEST_ATLAS_4=${f}/tmp/S002_Atlas4.nii.gz
-TEST_ATLAS_5=${f}/tmp/S002_Atlas5.nii.gz
-TEST_ATLAS_6=${f}/tmp/S002_Atlas6.nii.gz
-TEST_ATLAS_7=${f}/tmp/S002_Atlas7.nii.gz
-
 MD_FILENAME=${f}/MD_map.nii.gz
 
-staple_cmd=""
-for i in {2..7}; do
-
-MDSEG_FILENAME=${f}/pixyl_${i}.nii.gz
-#rm ${MDSEG_FILENAME}
-if [ ! -f ${MDSEG_FILENAME} ]; then
-#oxytc_test.py -i ${MD_FILENAME} -a ${f}/${CODE}_Atlas${i}.nii.gz -p roi_atlas${i}.pkl -o ${MDSEG_FILENAME} -m $THRESHOLD_MODE -devcyto $DEVCYTO -devvaso $DEVVASO
-oxytc_test.py -i ${MD_FILENAME} -a ${f}/${CODE}_Atlas${i}.nii.gz -p roi_atlas${i}.pkl -o ${MDSEG_FILENAME} -m $THRESHOLD_MODE 
-fi
-staple_cmd="${staple_cmd} ${MDSEG_FILENAME}"
-
-
+# Check if required files exist
+for file in "$PCSF" "$LFD" "$T1_SEG" "$SUPRATENTORIAL_MASK" "$MD_FILENAME"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: File $file not found!"
+        exit 1
+    fi
 done
 
+# Process each atlas
+staple_cmd=""
+for i in {2..7}; do
+    ATLAS=${f}/tmp/S002_Atlas${i}.nii.gz
+    MDSEG_FILENAME=${f}/pixyl_${i}.nii.gz
 
+    if [ ! -f "$ATLAS" ]; then
+        echo "Error: Atlas file $ATLAS not found!"
+        exit 1
+    fi
+
+    if [ ! -f "$MDSEG_FILENAME" ]; then
+        python oxytc_test.py -i "$MD_FILENAME" -a "$ATLAS" -p "roi_atlas${i}.pkl" -o "$MDSEG_FILENAME" -m "$THRESHOLD_MODE" -devcyto "$DEVCYTO" -devvaso "$DEVVASO"
+    fi
+
+    staple_cmd="${staple_cmd} ${MDSEG_FILENAME}"
+done
+
+# Run STAPLE algorithm
 c3d ${staple_cmd} -staple 1 -o ${f}/${CODE}_StapleSegmentation1_v1.nii.gz
 c3d ${staple_cmd} -staple 2 -o ${f}/${CODE}_StapleSegmentation2_v1.nii.gz
 
-
+# Invert test mask
 INV_TEST_MASK=${f}/${CODE}_InvTestMask.nii.gz
-c3d ${TEST_MASK} -threshold 0 0 1 0 -o ${INV_TEST_MASK}
+c3d ${SUPRATENTORIAL_MASK} -threshold 0 0 1 0 -o ${INV_TEST_MASK}
+
+# Final result
 RES=${f}/${CODE}_RO.nii.gz
 
-###Remove voxels near csf
-##stkRemoveObjects -i ${f}/${CODE}_StapleSegmentation1_v1.nii.gz -am ${INV_TEST_MASK} -v $VOLVASO -d 1 -o ${f}/${CODE}_StapleSegmentation1.nii.gz
-##stkRemoveObjects -i ${f}/${CODE}_StapleSegmentation2_v1.nii.gz -am ${INV_TEST_MASK} -v $VOLCYTO -d 1 -o ${f}/${CODE}_StapleSegmentation2.nii.gz
+# Remove voxels near CSF (commented out as per original script)
+# stkRemoveObjects -i ${f}/${CODE}_StapleSegmentation1_v1.nii.gz -am ${INV_TEST_MASK} -v $VOLVASO -d 1 -o ${f}/${CODE}_StapleSegmentation1.nii.gz
+# stkRemoveObjects -i ${f}/${CODE}_StapleSegmentation2_v1.nii.gz -am ${INV_TEST_MASK} -v $VOLCYTO -d 1 -o ${f}/${CODE}_StapleSegmentation2.nii.gz
 
-##MDSEG_FILENAME=${f}/${CODE}_Pixyl_Staple.nii.gz
-##c3d ${f}/${CODE}_StapleSegmentation1.nii.gz ${f}/${CODE}_StapleSegmentation2.nii.gz -scale 2 -add -o ${MDSEG_FILENAME}
-
-
-### GT_FILENAME=${f}/gt.nii.gz #ground true.
-##GT_CYTO_FILENAME=${f}/gt_cyto.nii.gz
-##GT_VASO_FILENAME=${f}/gt_vaso.nii.gz
-
-### c3d ${GT_FILENAME} -threshold 1 1 1 0 -o ${GT_VASO_FILENAME}
-### c3d ${GT_FILENAME} -threshold 2 2 1 0 -o ${GT_CYTO_FILENAME}
-
-###ImageValidationISLES ${GT_VASO_FILENAME} ${f}/${CODE}_StapleSegmentation1.nii.gz
-###ImageValidationISLES ${GT_CYTO_FILENAME} ${f}/${CODE}_StapleSegmentation2.nii.gz
-
-### senan:
-### c3d ${GT_FILENAME} ${MDSEG_FILENAME} -overlap 1 >> "Overlap1.csv"
-### c3d ${GT_FILENAME} ${MDSEG_FILENAME} -overlap 2 >> "Overlap2.csv"
-
-###pauline
-##c3d ${GT_CYTO_FILENAME} ${f}/${CODE}_StapleSegmentation2.nii.gz -overlap 1 -verbose >> ${CODE}"_Overlap_cyto.csv"
-##c3d ${GT_VASO_FILENAME} ${f}/${CODE}_StapleSegmentation1.nii.gz -overlap 1 -verbose >> ${CODE}"_Overlap_vaso.csv"
-
-exit
+# Exit script
+exit 0
