7f3b0406b77e35c2c58d3f690ff8b5878fe18b7e
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-03-04 10:15:03 +0100

    test: add first unit test for Subject creation

---
+++ oxytcmri/tests/test_unit_domain.py
@@ -0,0 +1,8 @@
+from oxytcmri.domain.entities.subject import Subject, SubjectType
+
+
+class TestSubject:
+    def test_create_subject(self):
+        new_subject = Subject.from_string_id("01-01-P")
+        assert new_subject.center_id == 1
+        assert new_subject.subject_type == SubjectType.PATIENT
