e3373917c668ef5dd04d931c960c2e9efa8dbbf2
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-03-14 14:48:31 +0100

    refactor(tests): create fixtures for TestNiftiVoxelData

--- oxytcmri/tests/unit/test_voxel_data_adapters.py
+++ oxytcmri/tests/unit/test_voxel_data_adapters.py
@@ -1,6 +1,7 @@
 """Tests for the NIfTI adapter implementations."""
 
 from pathlib import Path
+from pytest import fixture
 
 from oxytcmri.domain.entities.mri import VoxelData
 from oxytcmri.interface.mri.voxel_data_adapters import NiftiVoxelData
@@ -9,15 +10,18 @@ from oxytcmri.interface.mri.voxel_data_adapters import NiftiVoxelData
 class TestNiftiVoxelData:
     """Tests for the NiftiVoxelData adapter."""
     
-    def test_create_nifti_voxel_data(self):
-        """Test that we can create a NiftiVoxelData instance."""
-        test_file = Path(__file__).parent.parent/"test-data/dti-data/Healthy/C01/01_01v_mr_170913/MD_map.nii.gz"
+    @fixture
+    def md_map_file_path(self) -> Path:
+        file_path = Path(__file__).parent.parent / "test-data/dti-data/Healthy/C01/01_01v_mr_170913/MD_map.nii.gz"
+        if not file_path.exists():
+            raise FileNotFoundError(f"File not found: '{file_path}'")
+        return file_path
 
-        # Check file exists
-        assert test_file.exists()
-        
-        # Create NiftiVoxelData instance
-        voxel_data = NiftiVoxelData[float](test_file)
-        
+    @fixture
+    def nifti_voxel_data(self, md_map_file_path) -> NiftiVoxelData[float]:
+        return NiftiVoxelData[float](md_map_file_path)
+    
+    def test_create_nifti_voxel_data(self, nifti_voxel_data):
+        """Test that we can create a NiftiVoxelData instance."""        
         # Check instance type
-        assert isinstance(voxel_data, VoxelData)
+        assert isinstance(nifti_voxel_data, VoxelData)
