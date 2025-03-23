303dd98c522aac6542021166ed40e9b1f3351e69
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-03-23 14:33:32 +0100

    feat: check existence of base folder when initialize NiftiFoldersMRIExamRepository

--- oxytcmri/tests/unit/interface/test_unit_nifti_folders_mri_exam_repository.py
+++ oxytcmri/tests/unit/interface/test_unit_nifti_folders_mri_exam_repository.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+import pytest
 from oxytcmri.interface.repositories.nifti_folders_mri_exam_repository import NiftiFoldersMRIExamRepository
 
 
 class TestNiftiFoldersMRIExamRepository:
-    def test_create_niti_folders_instance(self):
-        nifti_folders = NiftiFoldersMRIExamRepository(
-            base_path="/path/to/base"
-        )
+    @pytest.fixture
+    def folder_base_path(self):
+        test_data_folder = Path(__file__).resolve().parents[2] / 'test-data'
+        return str(test_data_folder / "NiftiFoldersMRIExamRepository")
+
+    def test_inexistence_of_base_path(self):
+        # Test if ValueError is raised when the base path does not exist
+        with pytest.raises(ValueError):
+            NiftiFoldersMRIExamRepository("non/existent/path")
+
+    def test_create_niti_folders_instance(self, folder_base_path):
+        nifti_folders = NiftiFoldersMRIExamRepository(folder_base_path)
