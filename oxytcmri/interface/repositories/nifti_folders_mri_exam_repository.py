6baba9f205a118ecc56df41303d1f149b1b41306
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-03-23 14:18:01 +0100

    refactor: make NiftiFoldersMRIExamRepository inherit from MRIExamRepository

--- oxytcmri/interface/repositories/nifti_folders_mri_exam_repository.py
+++ oxytcmri/interface/repositories/nifti_folders_mri_exam_repository.py
@@ -1,7 +1,13 @@
-class NiftiFoldersMRIExamRepository:
+from oxytcmri.domain.entities.mri import MRIExam
+from oxytcmri.domain.ports.repositories import MRIExamRepository
+
+class NiftiFoldersMRIExamRepository(MRIExamRepository):
     def __init__(self, base_path):
         self.base_path = base_path
 
     def get_nifti_file_paths(self):
         # This method would contain logic to retrieve NIfTI file paths
+        pass
+
+    def get_exam_for_subject(self, subject_id: str) -> MRIExam:
         pass
