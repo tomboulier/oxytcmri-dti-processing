fc5c17c043e6e7521baa16a3175408e1e3272100
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-03-23 14:14:23 +0100

    feat: define class NiftiFoldersMRIExamRepository

---
+++ oxytcmri/interface/repositories/nifti_folders_mri_exam_repository.py
@@ -0,0 +1,7 @@
+class NiftiFoldersMRIExamRepository:
+    def __init__(self, base_path):
+        self.base_path = base_path
+
+    def get_nifti_file_paths(self):
+        # This method would contain logic to retrieve NIfTI file paths
+        pass
