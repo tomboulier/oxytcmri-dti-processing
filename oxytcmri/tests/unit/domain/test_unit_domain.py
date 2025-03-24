5cb0f6448e70a515104a3666568472c99da46a3c
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-03-24 13:59:02 +0100

    refactor(test): remove unused method in MockMaskData

--- oxytcmri/tests/unit/domain/test_unit_domain.py
+++ oxytcmri/tests/unit/domain/test_unit_domain.py
@@ -126,10 +126,6 @@ class MockMaskData(VoxelData[bool]):
     def get_voxel_volume_in_ml(self) -> float:
         return 8.0
 
-    def get_values_where(self, condition) -> List[bool]:
-        """Get values where condition is True."""
-        return [True, True, True]
-
 
 class MockInMemoryMRIRepository(MRIExamRepository):
     def __init__(self, atlases: List[Atlas]):
