82963e60624c45a6aa35ebad4e3248612dc7d0ad
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-04-23 17:44:25 +0200

    docs: restructure API and reference documentation

--- oxytcmri/domain/entities/subject.py
+++ oxytcmri/domain/entities/subject.py
@@ -1,3 +1,10 @@
+"""
+This module contains the `Subject` class which represents a subject in the Oxy-TC trial,
+as well as the associated value objects:
+
+- `SubjectType`: Represents the type of subject (e.g., "healthy volunteer", "patient");
+- `SubjectID`: Represents the unique identifier for a subject.
+"""
 from __future__ import annotations
 
 import re
