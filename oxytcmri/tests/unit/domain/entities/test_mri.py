62da0b79b387f949d00d3f8e9e34298fe520b6d7
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-04-28 09:30:28 +0200

    test(mri): relocate and refactor MRIExam and MRIExamId unit tests

--- oxytcmri/tests/unit/domain/entities/test_mri.py
+++ oxytcmri/tests/unit/domain/entities/test_mri.py
@@ -1,8 +1,42 @@
 import pytest
 
-from oxytcmri.domain.entities.mri import Atlas
+from oxytcmri.domain.entities.mri import Atlas, MRIExamId, MRIExam
+from oxytcmri.domain.entities.subject import SubjectId
 from oxytcmri.tests.unit.domain.mocks import MockSyntheticMRIExamRepository
 
+
+class TestMRIExamId:
+    @pytest.mark.parametrize("exam_id,expected_subject_id", [
+        ("06-78P-MR-170918", "06-78-P"),
+        ("10_73V_MR301015", "10-73-V"),
+        ("13-73P-190717", "13-73-P"),
+        ("01_74T_MR_101117", "01-74-T"),
+        ("23-69V-MR-220101", "23-69-V"),
+        ("11-73-VR", "11-73-V"),
+        ("18_71_V_GT", "18-71-V")
+    ])
+    def test_to_subject_id_valid_formats(self, exam_id, expected_subject_id):
+        assert MRIExamId(exam_id).to_subject_id() == SubjectId(expected_subject_id)
+
+    @pytest.mark.parametrize("invalid_id", [
+        "10-03X-MR301015",  # invalid subject type
+        "AB_CDZ_MR000000",  # totally invalid
+        "01-04-MR-191216",  # missing subject type
+        "MR-01-74P-191216",  # incorrect order
+        "xyz",  # too short
+    ])
+    def test_to_subject_id_invalid_formats_raise(self, invalid_id):
+        with pytest.raises(ValueError):
+            MRIExamId(invalid_id).to_subject_id()
+
+
+class TestMRIExam:
+    def test_mri_exam_id(self):
+        # Test if the MRIExamId is set correctly
+        string_exam_id = "06-78P-MR-170918"
+        mri_exam = MRIExam.from_string_exam_id(string_exam_id)
+        assert mri_exam.id == MRIExamId(string_exam_id)
+        assert mri_exam.subject_id == SubjectId("06-78-P")
 
 class TestAtlasSegmentation:
     @pytest.fixture
