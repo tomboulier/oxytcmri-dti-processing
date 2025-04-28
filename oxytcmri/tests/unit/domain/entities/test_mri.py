a87c0d7bd85b42dba0d0d8a863a5008f2dcb7f3b
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-04-28 09:35:20 +0200

    test: add docstrings for unit test methods in MRI entities

--- oxytcmri/tests/unit/domain/entities/test_mri.py
+++ oxytcmri/tests/unit/domain/entities/test_mri.py
@@ -16,6 +16,9 @@ class TestMRIExamId:
         ("18_01_V_GT", "18-01-V")
     ])
     def test_to_subject_id_valid_formats(self, exam_id, expected_subject_id):
+        """
+        Test if the MRIExamId is converted to the correct SubjectId.
+        """
         assert MRIExamId(exam_id).to_subject_id() == SubjectId(expected_subject_id)
 
     @pytest.mark.parametrize("invalid_id", [
@@ -26,13 +29,18 @@ class TestMRIExamId:
         "xyz",  # too short
     ])
     def test_to_subject_id_invalid_formats_raise(self, invalid_id):
+        """
+        Test if the MRIExamId raises ValueError for invalid formats.
+        """
         with pytest.raises(ValueError):
             MRIExamId(invalid_id).to_subject_id()
 
 
 class TestMRIExam:
     def test_mri_exam_id(self):
-        # Test if the MRIExamId is set correctly
+        """
+        Test if the MRIExamId is set correctly
+        """
         string_exam_id = "06-08P-MR-170918"
         mri_exam = MRIExam.from_string_exam_id(string_exam_id)
         assert mri_exam.id == MRIExamId(string_exam_id)
@@ -48,6 +56,9 @@ class TestAtlasSegmentation:
         return mri_exam.get_atlas_segmentation(atlas)
 
     def test_create_mask(self, atlas_segmentation):
+        """
+        Test if the mask is created correctly from the atlas segmentation.
+        """
         mask = atlas_segmentation.create_mask([29])
         assert mask is not None
         assert isinstance(mask.voxel_data.get_value_at(0, 0, 0), bool)
