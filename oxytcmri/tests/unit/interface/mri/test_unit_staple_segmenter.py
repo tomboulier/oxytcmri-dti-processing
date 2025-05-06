de464352d8b9ecc67e2ddc55aff5923eef2d8fe9
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-05-06 17:53:48 +0200

    refactor(tests): improve test structure with fixtures and reusable helper functions

--- oxytcmri/tests/unit/interface/mri/test_unit_staple_segmenter.py
+++ oxytcmri/tests/unit/interface/mri/test_unit_staple_segmenter.py
@@ -1,6 +1,8 @@
 """
 Unit tests for the STAPLE segmentation merger.
 """
+from typing import List
+
 import numpy
 import unittest
 import numpy as np
@@ -10,25 +12,31 @@ from unittest.mock import patch, MagicMock
 import pytest
 
 from oxytcmri.domain.entities.mri import DTIMap, DTIMetric, MRIExamId
-from oxytcmri.domain.use_cases.segment_dti_abnormal_values import DTIAbnormalValues, AbnormalValueType, AbnormalVoxelData
+from oxytcmri.domain.use_cases.segment_dti_abnormal_values import DTIAbnormalValues, AbnormalValueType, \
+    AbnormalVoxelData
 from oxytcmri.interface.mri.staple_segmenter import C3DSTAPLESegmentationMerger
 from oxytcmri.interface.mri.voxel_data_adapters import InMemoryNumpyVoxelData, NiftiVoxelData
 from oxytcmri.interface.mri.staple_segmenter import TemporaryNiftiIntegerVoxelData
 from oxytcmri.tests.fixtures import path_to_test_data_folder
 
 
+def get_md_map_nifti_voxel_data_for_subject_string(subject_string: str) -> NiftiVoxelData:
+    """Returns the path to the MD map NIfTI file."""
+    file_path = path_to_test_data_folder() / f"NiftiFoldersMRIExamRepository/{subject_string}/MD_map.nii.gz"
+    if not file_path.exists():
+        raise FileNotFoundError(f"File not found: '{file_path}'")
+    return NiftiVoxelData(file_path)
+
+
 class TestC3DSTAPLESegmentationMerger:
     """Test suite for C3DSTAPLESegmentationMerger."""
 
-    def test_merge(self):
-        """Test that merge() correctly merges the segmentations."""
-        dti_voxel_data = InMemoryNumpyVoxelData(
-            data=numpy.ones((3, 3, 3), dtype=numpy.float32),
-            voxel_volume=8.0,
-        )
+    @pytest.fixture
+    def segmentations(self) -> List[DTIAbnormalValues]:
+        nifti_voxel_data = get_md_map_nifti_voxel_data_for_subject_string("01_02t_mr_150328")
         dti_map = DTIMap(dti_metric=DTIMetric.from_acronym("FA"),
                          mri_exam_id=MRIExamId("02-01-t-mr-16022013"),
-                         voxel_data=dti_voxel_data)
+                         voxel_data=nifti_voxel_data)
 
         # create first segmentation
         segmentation_1 = DTIAbnormalValues.from_dti_map(dti_map=dti_map)
@@ -40,12 +48,14 @@ class TestC3DSTAPLESegmentationMerger:
         segmentation_2.voxel_data.set_value_at(0, 0, 1, AbnormalValueType.HIGH)
         segmentation_2.voxel_data.set_value_at(1, 0, 0, AbnormalValueType.LOW)
 
-        list_of_segmentations = [segmentation_1, segmentation_2]
+        return [segmentation_1, segmentation_2]
 
+    def test_merge(self, segmentations: List[DTIAbnormalValues]) -> None:
+        """Test that merge() correctly merges the segmentations."""
         # create the merger
         merger = C3DSTAPLESegmentationMerger()
         # merge the segmentations
-        merged_segmentation = merger.merge(list_of_segmentations)
+        merged_segmentation = merger.merge(segmentations)
 
         # check the merged segmentation
         assert isinstance(merged_segmentation, DTIAbnormalValues)
@@ -55,19 +65,11 @@ class TestTemporaryNiftiIntegerVoxelData:
     """Unit tests for TemporaryNiftiIntegerVoxelData."""
 
     @pytest.fixture
-    def md_map_file_path(self) -> Path:
-        """Returns the path to the MD map NIfTI file."""
-        file_path = path_to_test_data_folder() / "dti-data/Healthy/C01/01_01v_mr_170913/MD_map.nii.gz"
-        if not file_path.exists():
-            raise FileNotFoundError(f"File not found: '{file_path}'")
-        return file_path
-
-    @pytest.fixture
-    def abnormal_voxel_data(self, md_map_file_path: Path) -> AbnormalVoxelData:
+    def abnormal_voxel_data(self) -> AbnormalVoxelData:
         """Fixture to create a mock AbnormalVoxelData object."""
-        nifti_voxel_data = NiftiVoxelData(md_map_file_path)  # type: ignore
+        nifti_voxel_data = get_md_map_nifti_voxel_data_for_subject_string("01_02t_mr_150328")
         result = AbnormalVoxelData.from_voxel_data(nifti_voxel_data)
-        
+
         # Add some test values to the AbnormalVoxelData
         result.set_value_at(0, 0, 0, AbnormalValueType.LOW)
         result.set_value_at(1, 1, 1, AbnormalValueType.HIGH)
@@ -80,7 +82,7 @@ class TestTemporaryNiftiIntegerVoxelData:
         Test converting AbnormalVoxelData to TemporaryNiftiIntegerVoxelData.
         """
         temp_nifti = TemporaryNiftiIntegerVoxelData.from_abnormal_voxel_data(abnormal_voxel_data)
-        
+
         # assert that the dimensions and voxel volume are set correctly
         assert temp_nifti.get_dimensions() == abnormal_voxel_data.get_dimensions()
         assert temp_nifti.get_voxel_volume_in_ml() == abnormal_voxel_data.get_voxel_volume_in_ml()
